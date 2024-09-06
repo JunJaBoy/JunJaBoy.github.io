fe2dc0228319e6bcf6a5618290071dd91d1a55c2
junsuPark <krpjs0508@gmail.com>
2024-09-06 11:42:59 +0900

    jekyll

--- src/app/post/details/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PostDetailsPage() {
-    return <h1>This is post details</h1>
-}
--- src/app/post/list/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PostListPage() {
-    return <h1>Posts here!</h1>
-}
