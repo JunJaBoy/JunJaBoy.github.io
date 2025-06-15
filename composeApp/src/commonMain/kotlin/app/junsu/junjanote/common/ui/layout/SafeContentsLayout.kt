a3573860ea24ace1b549941c72ac8540db053868
junsuPark <krpjs0508@gmail.com>
2025-06-15 10:57:37 +0900

    safe layout

---
+++ composeApp/src/commonMain/kotlin/app/junsu/junjanote/common/ui/layout/SafeContentsLayout.kt
@@ -0,0 +1,11 @@
+package app.junsu.junjanote.common.ui.layout
+
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+fun Modifier.safeMaxWidth(): Modifier = this.then(
+    Modifier.widthIn(
+        max = 1000.dp,
+    ),
+)
