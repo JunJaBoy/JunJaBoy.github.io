43923e123a66e48f6406b3051d70d898ce55bbbd
junsuPark <krpjs0508@gmail.com>
2025-06-15 12:11:37 +0900

    sheet content

--- composeApp/src/commonMain/kotlin/app/junsu/junjanote/common/ui/PostSheet.kt
+++ composeApp/src/commonMain/kotlin/app/junsu/junjanote/common/ui/PostSheet.kt
@@ -1,6 +1,7 @@
 package app.junsu.junjanote.common.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,28 +11,41 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import app.junsu.junjanote.common.ui.corner.SmoothCornerShape
 
 fun LazyListScope.postSheetHeaderItem(
     key: Any? = null,
     contentType: Any? = null,
+    padding: PaddingValues = PaddingValues(all = 8.0.dp),
+    content: @Composable () -> Unit,
 ) {
     this.item(
         key = key,
         contentType = contentType,
     ) {
+        val cornerRadius = 32.0.dp
         Surface(
-            modifier = Modifier.fillMaxWidth().height(32.0.dp).clip(
+            modifier = Modifier.fillMaxWidth().background(
+                color = MaterialTheme.colorScheme.surfaceVariant,
                 shape = SmoothCornerShape(
-                    topLeft = 32.0.dp,
-                    topRight = 32.0.dp,
-                    smoothnessAsPercent = 0,
+                    topLeft = cornerRadius,
+                    topRight = cornerRadius,
                 ),
-            ),
-            content = {},
-            color = MaterialTheme.colorScheme.surfaceVariant,
-        )
+            ).padding(padding),
+            color = Color.Transparent,
+        ) {
+            Surface(
+                modifier = Modifier.fillMaxWidth().clip(
+                    shape = SmoothCornerShape(
+                        all = cornerRadius - padding.calculateTopPadding(),
+                    ),
+                ),
+                color = Color.Transparent,
+                content = content,
+            )
+        }
     }
 }
 
