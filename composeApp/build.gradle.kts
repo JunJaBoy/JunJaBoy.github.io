f649eb05a85b9744826bf5594e5c707407034a03
junsuPark <krpjs0508@gmail.com>
2025-06-15 14:31:27 +0900

    md support

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -74,6 +74,7 @@ kotlin {
             implementation(libs.coil.compose.core)
             implementation(libs.coil.compose)
             implementation(libs.coil.mp)
+            implementation(libs.markdown)
             implementation(libs.coil.network.ktor)
             implementation(libs.bundles.ktor)
             implementation(libs.ktor.client.cio.jvm)
